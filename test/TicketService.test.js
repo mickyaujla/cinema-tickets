7fe77bea79428a557d47fcae2a975650ad3411ae
Micky Aujla <micky.s.aujla@gmail.com>
2025-09-12 22:10:55 +0100

    Categorise tests to make them more readable

--- test/TicketService.test.js
+++ test/TicketService.test.js
@@ -1,4 +1,4 @@
-import { expect, jest } from '@jest/globals';
+import { describe, expect, jest } from '@jest/globals';
 import TicketService from '../src/pairtest/TicketService';
 import TicketTypeRequest from '../src/pairtest/lib/TicketTypeRequest.js';
 import InvalidPurchaseException from '../src/pairtest/lib/InvalidPurchaseException.js';
@@ -20,81 +20,89 @@ describe('TicketService', () => {
         jest.clearAllMocks();
     })
 
-    it.each([
+    describe('account validation', () => {
+        it.each([
         ['abc'],
         [1.5],
         [null],
         [-1],
-    ])('should throw an InvalidPurchaseException error when %s is given as the accountId', (input) => {
-        const adultTicket = new TicketTypeRequest(TicketType.ADULT, 1);
+        ])('should throw an InvalidPurchaseException error when %s is given as the accountId', (input) => {
+            const adultTicket = new TicketTypeRequest(TicketType.ADULT, 1);
 
-        expect(() => service.purchaseTickets(input, adultTicket))
-        .toThrow(new InvalidPurchaseException(ErrorMessages.INVALID_ACCOUNT_ID));
+            expect(() => service.purchaseTickets(input, adultTicket))
+            .toThrow(new InvalidPurchaseException(ErrorMessages.INVALID_ACCOUNT_ID));
 
-        expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
-        expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
+            expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
+            expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
+        });
     });
 
-    it('should throw an InvalidPurchaseException error when the Ticket Requests ticket count is <= 0', () => {
-        expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.ADULT, -1)))
-        .toThrow(new InvalidPurchaseException(ErrorMessages.TICKET_TYPE_MISSING_TICKETS));
+    describe('ticket request validation', () => {
+        it('should throw an InvalidPurchaseException error when the Ticket Requests ticket count is <= 0', () => {
+            expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.ADULT, -1)))
+            .toThrow(new InvalidPurchaseException(ErrorMessages.TICKET_TYPE_MISSING_TICKETS));
 
-        expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.ADULT, 0)))
-        .toThrow(new InvalidPurchaseException(ErrorMessages.TICKET_TYPE_MISSING_TICKETS));
+            expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.ADULT, 0)))
+            .toThrow(new InvalidPurchaseException(ErrorMessages.TICKET_TYPE_MISSING_TICKETS));
 
-        expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.ADULT, 1)))
-        .not.toThrow(new InvalidPurchaseException(ErrorMessages.TICKET_TYPE_MISSING_TICKETS));
+            expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.ADULT, 1)))
+            .not.toThrow(new InvalidPurchaseException(ErrorMessages.TICKET_TYPE_MISSING_TICKETS));
+        });
+
+        it('should throw InvalidPurchaseException if a TicketTypeRequest has an unknown type', () => {
+            const unknownTicketType = 'UNKNOWN_TYPE';
+            const fakeRequest = {
+                getTicketType: () => unknownTicketType,
+                getNoOfTickets: () => 1,
+            };
+
+            expect(() => service.purchaseTickets(1, fakeRequest))
+                .toThrow(new InvalidPurchaseException(`${ErrorMessages.UNKNOWN_TICKET_TYPE}: ${unknownTicketType}`));
+            
+            expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
+            expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
+        });
     });
 
-    it('should throw InvalidPurchaseException if a TicketTypeRequest has an unknown type', () => {
-        const unknownTicketType = 'UNKNOWN_TYPE';
-        const fakeRequest = {
-            getTicketType: () => unknownTicketType,
-            getNoOfTickets: () => 1,
-        };
+    describe('business rules validation', () => {
+        it('should throw an InvalidPurchaseException error when more than 25 tickets are purchased', () => {
+            expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.ADULT, 26)))
+            .toThrow(new InvalidPurchaseException(ErrorMessages.TOO_MANY_TICKETS));
 
-        expect(() => service.purchaseTickets(1, fakeRequest))
-            .toThrow(new InvalidPurchaseException(`${ErrorMessages.UNKNOWN_TICKET_TYPE}: ${unknownTicketType}`));
+            expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
+            expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
+        });
+
+        it('should throw an InvalidPurchaseException error when there are no adult tickets', () => {
+            expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.CHILD, 5)))
+            .toThrow(new InvalidPurchaseException(ErrorMessages.NO_ADULT_TICKETS));
+
+            expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
+            expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
+        });
         
-        expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
-        expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
+        it('should throw an InvalidPurchaseException error when there are more infants than adults', () => {
+            const requests = [new TicketTypeRequest(TicketType.ADULT, 10), new TicketTypeRequest(TicketType.INFANT, 15)]
+            
+            expect(() => service.purchaseTickets(1, ...requests))
+            .toThrow(new InvalidPurchaseException(ErrorMessages.MORE_INFANTS_THAN_ADULTS));
+
+            expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
+            expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
+        });
     });
 
-    it('should throw an InvalidPurchaseException error when more than 25 tickets are purchased', () => {
-        expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.ADULT, 26)))
-        .toThrow(new InvalidPurchaseException(ErrorMessages.TOO_MANY_TICKETS));
+    describe('successful purchase', () => {
+        it('should make payment and reserve seats when successfully validating all the business rules', () => {
+            let adultCount = 10, infantCount = 5, childCount = 5;
+            const requests = [new TicketTypeRequest(TicketType.ADULT, adultCount), new TicketTypeRequest(TicketType.INFANT, infantCount), new TicketTypeRequest(TicketType.CHILD, childCount)]
+            const totalCost = adultCount * TicketPrices[TicketType.ADULT] + childCount * TicketPrices[TicketType.CHILD]
+            
+            expect(() => service.purchaseTickets(1, ...requests))
+            .not.toThrow(InvalidPurchaseException);
 
-        expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
-        expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
-    })
-
-    it('should throw an InvalidPurchaseException error when there are no adult tickets', () => {
-        expect(() => service.purchaseTickets(1, new TicketTypeRequest(TicketType.CHILD, 5)))
-        .toThrow(new InvalidPurchaseException(ErrorMessages.NO_ADULT_TICKETS));
-
-        expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
-        expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
-    })
-    
-    it('should throw an InvalidPurchaseException error when there are more infants than adults', () => {
-        const requests = [new TicketTypeRequest(TicketType.ADULT, 10), new TicketTypeRequest(TicketType.INFANT, 15)]
-        
-        expect(() => service.purchaseTickets(1, ...requests))
-        .toThrow(new InvalidPurchaseException(ErrorMessages.MORE_INFANTS_THAN_ADULTS));
-
-        expect(TicketPaymentService.prototype.makePayment).not.toHaveBeenCalled();
-        expect(SeatReservationService.prototype.reserveSeat).not.toHaveBeenCalled();
-    })
-
-    it('should make payment and reserve seats when successfully validating all the business rules', () => {
-        let adultCount = 10, infantCount = 5, childCount = 5;
-        const requests = [new TicketTypeRequest(TicketType.ADULT, adultCount), new TicketTypeRequest(TicketType.INFANT, infantCount), new TicketTypeRequest(TicketType.CHILD, childCount)]
-        const totalCost = adultCount * TicketPrices[TicketType.ADULT] + childCount * TicketPrices[TicketType.CHILD]
-        
-        expect(() => service.purchaseTickets(1, ...requests))
-        .not.toThrow(InvalidPurchaseException);
-
-        expect(TicketPaymentService.prototype.makePayment).toHaveBeenCalledWith(1, totalCost);
-        expect(SeatReservationService.prototype.reserveSeat).toHaveBeenCalledWith(1, (adultCount + childCount));
-    })
+            expect(TicketPaymentService.prototype.makePayment).toHaveBeenCalledWith(1, totalCost);
+            expect(SeatReservationService.prototype.reserveSeat).toHaveBeenCalledWith(1, (adultCount + childCount));
+        });
+    });
 })
